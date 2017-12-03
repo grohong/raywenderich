aa108f5a67429f8b490324c0a90640eb5ccfed59
Seong ho Hong <grohong76@gmail.com>
2017-12-03 14:50:47 +0900

    trie implement 구현

--- Swift Algorism/Swift Trie Data Structure/MyPlayground.playground/Contents.swift
+++ Swift Algorism/Swift Trie Data Structure/MyPlayground.playground/Contents.swift
@@ -1,5 +1,53 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+class TrieNode<T: Hashable> {
+    var value: T?
+    weak var parent: TrieNode?
+    var children: [T: TrieNode] = [:]
+    var isTerminating = false
 
-var str = "Hello, playground"
+    
+    init(value: T? = nil, parent: TrieNode? = nil) {
+        self.value = value
+        self.parent = parent
+    }
+    
+    func add(child: T) {
+        guard children[child] == nil else { return }
+        
+        children[child] = TrieNode(value: child, parent: self)
+    }
+}
+
+class Trie {
+    typealias Node = TrieNode<Character>
+    fileprivate let root: Node
+    
+    init() {
+        root = Node()
+    }
+}
+
+extension Trie {
+    func insert(word: String) {
+        guard !word.isEmpty else { return }
+        
+        var currentNode = root
+        
+        let characters = Array(word.lowercased().characters)
+        var currentIndex = 0
+        
+        while currentIndex < characters.count {
+            let character = characters[currentIndex]
+            
+            if let child = currentNode.children[character] {
+                currentNode = child
+            } else {
+                currentNode.add(child: character)
+                currentNode = currentNode.children[character]!
+            }
+            
+            currentIndex += 1
+        }
+    }
+}
