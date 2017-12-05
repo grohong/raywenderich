48ef4e7dec4f407d0c64017d67213ebf165884f2
Seong ho Hong <grohong76@gmail.com>
2017-12-05 20:48:22 +0900

    linked list example 추가

--- Swift Algorism/Swift Linked List Data Structure/MyPlayground.playground/Contents.swift
+++ Swift Algorism/Swift Linked List Data Structure/MyPlayground.playground/Contents.swift
@@ -1,32 +1,32 @@
 //: Playground - noun: a place where people can play
 
-public class Node {
-    var value: String
-    var next: Node?
-    weak var previous: Node?
+public class Node<T> {
+    var value: T
+    var next: Node<T>?
+    weak var previous: Node<T>?
     
-    init(value: String) {
+    init(value: T) {
         self.value = value
     }
 }
 
-public class LinkedList {
-    fileprivate var head: Node?
-    private var tail: Node?
+public class LinkedList<T> {
+    fileprivate var head: Node<T>?
+    private var tail: Node<T>?
     
     public var isEmpty: Bool {
         return head == nil
     }
     
-    public var first: Node? {
+    public var first: Node<T>? {
         return head
     }
     
-    public var last: Node? {
+    public var last: Node<T>? {
         return tail
     }
     
-    public func append(value: String) {
+    public func append(value: T) {
         let newNode = Node(value: value)
         
         if let tailNode = tail {
@@ -37,6 +37,47 @@ public class LinkedList {
         }
         
         tail = newNode
+    }
+    
+    public func nodeAt(index: Int) -> Node<T>? {
+        if index >= 0 {
+            var node = head
+            var i = index
+            
+            while node != nil {
+                if i == 0 { return node }
+                i -= 1
+                node = node!.next
+            }
+        }
+        
+        return nil
+    }
+    
+    public func removeAll() {
+        head = nil
+        tail = nil
+    }
+    
+    public func remove(node: Node<T>) -> T {
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next
+        } else {
+            head = next
+        }
+        next?.previous = prev
+        
+        if next == nil {
+            tail = prev
+        }
+        
+        node.previous = nil
+        node.next = nil
+        
+        return node.value
     }
 }
 
@@ -55,7 +96,7 @@ extension LinkedList: CustomStringConvertible {
     }
 }
 
-let dogBreeds = LinkedList()
+let dogBreeds = LinkedList<String>()
 
 dogBreeds.append(value: "Labrador")
 dogBreeds.append(value: "Bulldog")
@@ -63,3 +104,12 @@ dogBreeds.append(value: "Beagle")
 dogBreeds.append(value: "Husky")
 
 print(dogBreeds)
+print(dogBreeds.nodeAt(index: 2)?.value)
+
+dogBreeds.removeAll()
+print(dogBreeds)
+
+let numbers = LinkedList<Int>()
+numbers.append(value: 5)
+numbers.append(value: 10)
+numbers.append(value: 15)
